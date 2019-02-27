259dff29685e4a28824d9a78b98574d0a60d85f4
Gyan Parkash <gyan.p@morrisdigital.mobi>
2019-02-27 16:36:37 +0530

    splach screen icon changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { IonicModule} from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { ChooseLangComponent } from './components/choose-lang/choose-lang.component';
 import { SearchComponent } from './components/search/search.component';
@@ -15,6 +16,7 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   imports: [
     CommonModule,
+    IonicModule,
     BrowserModule,
   ],
   exports: [
